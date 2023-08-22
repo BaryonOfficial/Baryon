3e3160cdecf0c561e6abfaa6dea377802d87a422
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2023-08-22 22:35:40 +0200

    many updates

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 import * as THREE from 'three';
-import * as essentia from 'essentia.js';
-import Perlin from 'perlin.js';
 import * as math from 'mathjs';
+import { Noise } from 'noisejs';
 
+import Essentia from 'https://cdn.jsdelivr.net/npm/essentia.js@0.1.3/dist/essentia.js-core.es.js';
+// import essentia-wasm-module
+import { EssentiaWASM } from 'https://cdn.jsdelivr.net/npm/essentia.js@0.1.3/dist/essentia-wasm.es.js';
 
-// Constants
-const NUM_PARTICLES = 6144;
-const SCALE_FACTOR = 50;
-const math = require('mathjs');
+const essentia = new Essentia(EssentiaWASM);
+
+// prints version of essentia wasm backend
+console.log(essentia.version)
+
+// prints all the available algorithm methods in Essentia
+console.log(essentia.algorithmNames)
 
 // Scene, Camera, Renderer Initialization
 const scene = new THREE.Scene();
@@ -17,36 +22,43 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Other constants
+const NUM_PARTICLES = 6144;
+const SCALE_FACTOR = 50;
+const noise = new Noise(Math.random());
+
+const particlesMaterial = new THREE.PointsMaterial({ size: 0.1, vertexColors: true });
+
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', initializeAudio);
+
 // Particle Geometry Initialization
 const particlesGeometry = new THREE.BufferGeometry();
 const particleVertices = new Float32Array(NUM_PARTICLES * 3);
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particleVertices, 3));
-const particlesMaterial = new THREE.PointsMaterial({ size: 0.1, vertexColors: true });
-
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
+const particleColors = new Float32Array(NUM_PARTICLES * 3);
+
 
 for (let i = 0; i < NUM_PARTICLES; i++) {
   // Use Perlin noise to get values
-  const x = (perlin.noise(i, 0, 0) * 2 - 1) * 10;
-  const y = (perlin.noise(0, i, 0) * 2 - 1) * 10;
-  const z = (perlin.noise(0, 0, i) * 2 - 1) * 10;
+  const x = (noise.perlin3(i, 0, 0) * 2 - 1) * 10; // Check usage based on the library
+  const y = (noise.perlin3(0, i, 0) * 2 - 1) * 10;
+  const z = (noise.perlin3(0, 0, i) * 2 - 1) * 10;
 
   particleVertices[i * 3] = x;
   particleVertices[i * 3 + 1] = y;
   particleVertices[i * 3 + 2] = z;
 }
 particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particleVertices, 3));
-
-
-// Create a color attribute for particles
-const particleColors = new Float32Array(NUM_PARTICLES * 3);
 particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particleColors, 3));
 
-// Audio Analysis Initialization
-const essentiaInstance = new essentia.Essentia();
-
 let audioContext = new AudioContext();
+
+if (audioContext.state === 'suspended') {
+  audioContext.resume();
+}
+
 let audioSource = audioContext.createBufferSource();
 let audioAnalyser = audioContext.createAnalyser();
 
@@ -72,8 +84,6 @@ function initializeAudio() {
       })
       .catch(error => console.error('An error occurred:', error));
   }
-  // Call the function to initialize the audio
-  initializeAudio();
 
 // Frequencies for notes in the 4th octave (C4 to B4)
 const noteFrequencies = {
@@ -131,20 +141,27 @@ const noteFrequencies = {
     if (octave == 6) return 1; // Full luminosity for octave 6
     return 0; // Default to black for out-of-range octaves
   }
-  
+    
   function mapNoteToColor(noteWithOctave) {
-    const note = noteWithOctave.slice(0, -1);
-    const octave = parseInt(noteWithOctave.slice(-1));
+    const noteRegex = /^[A-G]#?/;
+    const note = noteWithOctave.match(noteRegex)[0];
+    const octave = parseInt(noteWithOctave.slice(note.length));
     const luminosityFactor = getLuminosityFactor(octave);
   
     // Get the base color
     let color = noteColors[note];
+    
+    if (!color) {
+      console.error('Color not found for note:', note);
+      return [0, 0, 0]; // Default color if note is not found
+    }
   
     // Apply luminosity factor
     color = color.map(channel => channel * luminosityFactor);
   
     return color;
-  }  
+  }
+  
   
   // Example usage:
   const frequency = 440; // A4
@@ -160,13 +177,18 @@ function animate() {
     // Audio Analysis
     let spectrum = new Uint8Array(audioAnalyser.frequencyBinCount);
     audioAnalyser.getByteFrequencyData(spectrum);
-    let audioSignal = essentiaInstance.arrayToVector(spectrum); // Corrected variable name
-    let pitch = essentiaInstance.PitchYinFFT(audioSignal); // Needs preprocessing of audio signal
+    let audioSignal = essentia.arrayToVector(spectrum); // Corrected variable name
+    let pitch = essentia.PitchYinFFT(audioSignal); // Needs preprocessing of audio signal
     let note = mapFrequencyToNote(pitch.freq); // Assuming pitch.freq is a frequency
     let color = mapNoteToColor(note);
-
-    
+    const volume = essentia.Energy(audioSignal);
   
+    const l = mapToL(pitch);
+    const m = mapToM(volume);
+    const theta = mapToTheta(pitch, volume);
+    const phi = mapToPhi(pitch, volume);
+    const harmonicValue = computeSphericalHarmonic(l, m, theta, phi);
+    
     // Update particles based on audio analysis
     updateParticles(particleVertices, pitch.freq, color);
   
@@ -182,24 +204,13 @@ function updateParticles(particleVertices, pitch, color) {
       let x = particleVertices[index];
       let z = particleVertices[index + 2];
       let cymaticsEffect = cymaticsFunction(x, z, pitch % 10, (pitch + 2) % 10);
+      
+      // Check if cymaticsEffect is NaN
+      if (isNaN(cymaticsEffect)) {
+      console.error("NaN detected in cymaticsEffect:", x, z, pitch);
+      continue; // Skip to the next iteration
+    }
       particleVertices[index + 1] = cymaticsEffect * SCALE_FACTOR;
-
-    const pitch = getPitch(audioData);
-    const volume = getVolume(audioData);
-    const l = mapToL(pitch);
-    const m = mapToM(volume);
-    const theta = mapToTheta(pitch, volume);
-    const phi = mapToPhi(pitch, volume);
-  
-    const harmonicValue = computeSphericalHarmonic(l, m, theta, phi);
-  
-    particles.forEach((particle) => {
-      particle.position.x = harmonicValue.re; // Real part for x
-      particle.position.y = harmonicValue.im; // Imaginary part for y
-      // Modify other properties as needed
-    });
-  
-    render();
   
       // Update color based on the note
       colors[index] = color[0] / 255; // Red component
@@ -209,26 +220,39 @@ function updateParticles(particleVertices, pitch, color) {
     particlesGeometry.attributes.position.needsUpdate = true; // Notify three.js that vertices have changed
     particlesGeometry.attributes.color.needsUpdate = true; // Notify three.js that colors have changed
 
-
   }
-  
-  function updateParticles(audioData) {
-    
-  }
-  
   
 // Cymatics function
 function cymaticsFunction(x, z, n, m) {
   return Math.cos(n * x * Math.PI) * Math.cos(m * z * Math.PI) - Math.cos(m * x * Math.PI) * Math.cos(n * z * Math.PI);
 }
 
-function computeSphericalHarmonic(l, m, theta, phi) {
-    const factor1 = Math.sqrt((2 * l + 1) / (4 * Math.PI) * (math.factorial(l - m) / math.factorial(l + m)));
-    const factor2 = math.legendreP(l, m, Math.cos(theta));
-    const factor3 = math.exp(math.complex(0, m * phi));
-    return factor1 * factor2 * factor3;
+function legendreP(l, m, x) {
+  if (m < 0) return 0;
+  if (m === 0 && l === 0) return 1;
+  if (l < m) return 0;
+  let p0 = 1;
+  let p1 = x;
+  for (let i = 2; i <= l; i++) {
+    let p2 = ((2 * i - 1) * x * p1 - (i + m - 1) * p0) / (i - m);
+    p0 = p1;
+    p1 = p2;
   }
-  
+  let fact = 1;
+  for (let i = 1; i <= m; i++) {
+    fact *= -fact * (2 * i - 1);
+  }
+  return fact * p1;
+}
+
+
+function computeSphericalHarmonic(l, m, theta, phi) {
+  const factor1 = Math.sqrt((2 * l + 1) / (4 * Math.PI) * (math.factorial(l - m) / math.factorial(l + m)));
+  const factor2 = legendreP(l, m, Math.cos(theta)); // Changed to custom legendreP
+  const factor3 = math.exp(math.complex(0, m * phi));
+  return factor1 * factor2 * factor3;
+}
+
   
   function mapToL(pitch) {
     return pitch / 100;
