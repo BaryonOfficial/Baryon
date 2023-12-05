18568b71cd5185015ac77be25239f170224172e1
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2023-12-05 23:54:10 +0100

    working 3D chladni

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import GUI from 'lil-gui';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { createNoise2D } from 'simplex-noise';
-const noise2D = createNoise2D();
+import { createNoise3D } from 'simplex-noise';
+const noise3D = createNoise3D();
 
 // Variables
+
 let parameters, particlesGeometry, particlesMaterial, particlePoints;
+let noiseOffset = 0.1; // Adjust this value to change the amount of "wobble"
+let damping = 0.9; // Adjust this value to change the rate of damping
 const pi = Math.PI;
 
 // Debug
@@ -46,7 +49,7 @@ window.addEventListener('resize', () => {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 8;
+camera.position.z = 10;
 scene.add(camera);
 
 // Controls
@@ -63,20 +66,36 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Chladni pattern function
-const chladni = (x, y, a, b, m, n) => 
-  a * Math.sin(pi * n * x) * Math.sin(pi * m * y) + b * Math.sin(pi * m * x) * Math.sin(pi * n * y);
+const chladni = (x, y, z, N, parameters) => {
+  let sum = 0;
+  for (let i=0; i < N; i++) {
+    const Ai = parameters.waveComponents[i][`A${i}`]
+    const ui = parameters.waveComponents[i][`u${i}`]
+    const vi = parameters.waveComponents[i][`v${i}`]
+    const wi = parameters.waveComponents[i][`w${i}`]
+    sum += Ai * Math.sin(ui * pi * x) * Math.sin(vi * pi * y) * Math.sin(wi * pi * z)
+  }
+  return sum
+}
 
-// Parameters for particles
 parameters = {
-  m: 8, // freq param 1
-  n: 4, // freq param 2
-  a: 1, // freq param 3
-  b: 1, // freq param 4
-  v: 0.15, // velocity
-  num: 25000, // number of particles
-};
+  N: 10,
+  v: 0.15,
+  num: 25000,
+  waveComponents:[]
+}
 
-const planeSize = 10; // Example size of the plane
+// Populate initial wave component values
+for (let i = 0; i < parameters.N; i++) {
+  parameters.waveComponents.push({
+    [`A${i}`]: Math.floor(Math.random() * 10) + 1,
+    [`u${i}`]: Math.floor(Math.random() * 10) + 1,
+    [`v${i}`]: Math.floor(Math.random() * 10) + 1,
+    [`w${i}`]: Math.floor(Math.random() * 10) + 1
+  });
+}
+
+const sphereRadius = 5; // Radius of the sphere
 
 const setupParticles = () => {
   // Create a BufferGeometry for particles
@@ -88,7 +107,7 @@ const setupParticles = () => {
 
 // Create a PointsMaterial for particles with size, sizeAttenuation, and blending properties
   particlesMaterial = new THREE.PointsMaterial({
-    size: 0.2, // Adjusted particle size with visible light trail effect
+    size: 0.05, // Adjusted particle size with visible light trail effect
     sizeAttenuation: true,
     vertexColors: true, // Enable vertex colors
     blending: THREE.AdditiveBlending, // Additive blending for light effect
@@ -98,15 +117,19 @@ const setupParticles = () => {
 
   // Initialize positions and colors
   for (let i = 0; i < parameters.num; i++) {
-    const i3 = i * 3;
+    const i3 = i * 3
+    const theta = Math.random() * 2 * pi + noise3D(i, i, i)
+    const phi = Math.acos(2 * Math.random() - 1) + noise3D(i, i, i)
+    const r = sphereRadius * Math.pow(Math.random(), 1/3);
 
     // Initialize positions
-    positions[i3]     = (Math.random() - 0.5) * planeSize;
-    positions[i3 + 1] = (Math.random() - 0.5) * planeSize;
-    positions[i3 + 2] = 0;
+    positions[i3]     = r * Math.sin(phi) * Math.cos(theta);
+    positions[i3 + 1] = r * Math.sin(phi) * Math.sin(theta);
+    positions[i3 + 2] = r * Math.cos(phi);
 
     // New color assignment, you could also vary this to create a gradient
-    const hue = (1.0 + noise2D(positions[i3], positions[i3 + 1])) / 2.0; // Use noise for color variation
+    const zNormalized = (positions[i3 + 2] + sphereRadius) / (2 * sphereRadius);
+    const hue = zNormalized
     const color = new THREE.Color().setHSL(hue, 1.0, 0.5); // Adjust hue, saturation, lightness for laser color
     colors[i3]     = color.r;
     colors[i3 + 1] = color.g;
@@ -117,12 +140,6 @@ const setupParticles = () => {
   particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
   particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
-  // Create a PointsMaterial for particles, enabling vertex colors
-  particlesMaterial = new THREE.PointsMaterial({
-    size: 0.05,
-    vertexColors: true // Enable vertex colors
-  });
-
   // Create a Points object using the geometry and material
   particlePoints = new THREE.Points(particlesGeometry, particlesMaterial);
   scene.add(particlePoints);
@@ -131,38 +148,49 @@ const setupParticles = () => {
 
 // Update particle positions
 const updateParticles = () => {
+
+  console.log('Updating particles...');
+
   const positions = particlesGeometry.attributes.position.array;
   const minWalk = 0.001;
   
   for (let i = 0; i < parameters.num; i++) {
     const i3 = i * 3;
     
-    // Convert positions for Chladni function
-    let x = (positions[i3] / planeSize + 0.5);
-    let y = (positions[i3 + 1] / planeSize + 0.5);
-
-    // Calculate Chladni pattern value
-    let chladniValue = chladni(x, y, parameters.a, parameters.b, parameters.m, parameters.n);
+    // Calculate Chladni pattern value with a noise offset
+    let x = (positions[i3] / sphereRadius);
+    let y = (positions[i3 + 1] / sphereRadius);
+    let z = (positions[i3 + 2] / sphereRadius);
+    let chladniValue = chladni(x, y, z, parameters.N, parameters);
     let stochasticAmplitude = parameters.v * Math.abs(chladniValue)
 
     // Ensure min movement
     stochasticAmplitude = Math.max(stochasticAmplitude, minWalk)
 
-    // Update positions based on Chladni value (can add random component as well)
-    positions[i3] += (Math.random() - 0.5) * stochasticAmplitude * 2;
-    positions[i3 + 1] += (Math.random() - 0.5) * stochasticAmplitude * 2;
-    
-
-    // Handle boundaries more naturally
-    if (positions[i3] < -planeSize / 2) positions[i3] = -planeSize / 2;
-    if (positions[i3] > planeSize / 2) positions[i3] = planeSize / 2;
-    if (positions[i3 + 1] < -planeSize / 2) positions[i3 + 1] = -planeSize / 2;
-    if (positions[i3 + 1] > planeSize / 2) positions[i3 + 1] = planeSize / 2;
+    // Update positions based on Chladni value (using Math.random)
+    positions[i3] += ((Math.random() - 0.5) * stochasticAmplitude * 2);
+    positions[i3 + 1] += ((Math.random() - 0.5) * stochasticAmplitude * 2);
+    positions[i3 + 2] += ((Math.random() - 0.5) * stochasticAmplitude * 2);
+   
+    // Keep particles within the sphere
+    const distance = Math.sqrt(positions[i3]**2 + positions[i3 + 1]**2 + positions[i3 + 2]**2)
+    if (distance > sphereRadius) {
+      const scale = sphereRadius /distance;
+      positions[i3] *= scale
+      positions[i3 + 1] *= scale
+      positions[i3 + 2] *= scale
+    }
   }
 
   particlesGeometry.attributes.position.needsUpdate = true;
 };
 
+let rotationSpeed = .01;
+
+// Add GUI control for rotation speed
+gui.add({ rotationSpeed: rotationSpeed }, 'rotationSpeed').min(0).max(0.1).step(0.001).onChange(value => {
+  rotationSpeed = value;
+});
 
 // Initialize and start animation loop
 function init() {
@@ -172,6 +200,10 @@ function init() {
 
 const tick = () => {
   controls.update();
+
+  // Rotate the entire shape
+  particlePoints.rotation.y += rotationSpeed
+
   updateParticles();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
@@ -179,31 +211,6 @@ const tick = () => {
 
 init();
 
-// GUI controllers
-gui.add(parameters, 'm').min(1).max(16).step(1).onChange(updateParticles);
-gui.add(parameters, 'n').min(1).max(16).step(1).onChange(updateParticles);
-gui.add(parameters, 'a').min(-2).max(2).step(1).onChange(updateParticles);
-gui.add(parameters, 'b').min(-2).max(2).step(1).onChange(updateParticles);
-gui.add(parameters, 'v').min(0.01).max(.2).step(0.01).onChange(updateParticles);
-
-// GUI controller for 'num' with onFinishChange
-gui.add(parameters, 'num').min(2000).max(30000).step(1000).onFinishChange(() => {
-  // Dispose of the old geometry and material to free up GPU memory
-  if (particlePoints) {
-      particlePoints.geometry.dispose();
-      particlePoints.material.dispose();
-      scene.remove(particlePoints);
-
-      // Clear references
-      particlePoints = null;
-      particlesGeometry = null;
-      particlesMaterial = null;
-  }
-
-  // Re-setup the particles with the new count
-  setupParticles();
-
-  // Re-add particlePoints to the scene
-  renderer.render(scene, camera)
+gui.add({ noiseOffset: noiseOffset }, 'noiseOffset').min(0).max(1).step(0.01).onChange(value => {
+  noiseOffset = value;
 });
-
