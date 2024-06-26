edb51c6b48b66bfc12f92cbd1fb881497b26e4c9
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2024-06-27 01:12:58 +0200

    website update and signup form code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Story from './components/Story';
 import Plugin from './components/Plugin';
+import PluginSignUp from './components/PluginSignUp';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/plugin" element={<Plugin />} />
         <Route path="/story" element={<Story />} />
+        <Route path="/signup" element={<PluginSignUp />} />
       </Routes>
     </Router>
   );
