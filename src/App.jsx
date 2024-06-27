c01e9722b825050f48f67b8bb1b2138b9d34adaf
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2024-06-27 16:44:28 +0200

    removed signup route from testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Story from './components/Story';
 import Plugin from './components/Plugin';
-import { SignUpForm } from './components/PluginSignUp';
 import './App.css';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/plugin" element={<Plugin />} />
         <Route path="/story" element={<Story />} />
-        <Route path="/signup" element={<SignUpForm />} />
       </Routes>
     </Router>
   );
