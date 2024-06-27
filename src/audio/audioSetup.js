1aa4e5439f3a9c3cd7cd05a778fd1049ed65ddca
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2024-06-27 16:06:44 +0200

    fixed bug with exports

--- src/audio/audioSetup.js
+++ src/audio/audioSetup.js
@@ -346,9 +346,9 @@ export function setupAudioGraph() {
     alert('SharedArrayBuffer is not supported in this browser. Please use a compatible browser.');
     return;
   }
-  let sab = exports.RingBuffer.getStorageForCapacity(audioObject.capacity, Float32Array); // capacity: three float32 values [pitch, confidence, rms]
-  let rb = new exports.RingBuffer(sab, Float32Array);
-  audioObject.audioReader = new exports.AudioReader(rb);
+  let sab = window.exports.RingBuffer.getStorageForCapacity(audioObject.capacity, Float32Array); // capacity: three float32 values [pitch, confidence, rms]
+  let rb = new window.exports.RingBuffer(sab, Float32Array);
+  audioObject.audioReader = new window.exports.AudioReader(rb);
 
   audioObject.essentiaNode = new AudioWorkletNode(audioObject.audioCtx, 'audio-data-processor', {
     processorOptions: {
