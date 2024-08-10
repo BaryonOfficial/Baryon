56cf5b840aa847c3a12d60ba9436ead1583f1f7b
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2024-08-10 02:39:05 +0200

    added vercel speedinsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Homepage from './components/Homepage';
 import Story from './components/Story';
 import Plugin from './components/Plugin';
+
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/plugin" element={<Plugin />} />
-        <Route path="/story" element={<Story />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/plugin" element={<Plugin />} />
+          <Route path="/story" element={<Story />} />
+        </Routes>
+      </Router>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
 
