ee671c9302692e8b83a9774c8ce10d5db8f2bdcc
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2024-11-01 05:57:00 +0100

    fixed eslint rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
