10e1a8acc8a8ab9762c3911971845841387329ca
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2024-11-08 20:27:37 +0100

    postprocessing component

---
+++ src/components/Scene/Effects.tsx
@@ -0,0 +1,27 @@
+import { ToneMapping, EffectComposer, Bloom, SMAA } from '@react-three/postprocessing';
+import { ToneMappingMode } from 'postprocessing';
+
+import { useThree } from '@react-three/fiber';
+
+interface EffectsProps {
+  bloomStrength?: number;
+  bloomRadius?: number;
+  bloomThreshold?: number;
+}
+
+export function Effects({
+  bloomStrength = 0.36,
+  bloomRadius = -2.0,
+  bloomThreshold = 0.4,
+}: EffectsProps) {
+  const { gl } = useThree();
+  const isLowPerformance = gl.getPixelRatio() === 1 && !gl.capabilities.isWebGL2;
+
+  return (
+    <EffectComposer>
+      <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
+      <Bloom intensity={bloomStrength} radius={bloomRadius} luminanceThreshold={bloomThreshold} />
+      {isLowPerformance ? <SMAA /> : <></>}
+    </EffectComposer>
+  );
+}
