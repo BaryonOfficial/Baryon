93d36503f349d16a1bf95ea2445a2f7763e58418
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2025-01-31 19:15:00 +0100

    chore: update docs

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -116,7 +116,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Baryon Project. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Baryon`,
     },
     prism: {
       theme: prismThemes.github,
