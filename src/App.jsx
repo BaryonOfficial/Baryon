de809e62b1ebac549efaf83e54c55feffa28bb6d
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2025-02-04 20:15:34 +0100

    chore: revert most files to js,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import ThreeScene from './components/Scene/ThreeScene';
+import ThreeScene from './components/Scene/ThreeScene.jsx';
 
 import './App.css';
 
