a4b8607491db6004864ae2ed0f4bd1a5531eb6a9
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2025-05-08 12:01:25 +0200

    fix: remove strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -7,7 +6,7 @@ const root = document.getElementById('root');
 if (!root) throw new Error('Root element not found');
 
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
 );
