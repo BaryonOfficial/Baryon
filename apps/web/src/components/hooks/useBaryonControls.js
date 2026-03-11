9c11d9bbfbf4e4380d0e0789abeb010e2283aaca
Anon B <114008947+RektTillNoon@users.noreply.github.com>
2026-03-11 00:32:33 -0400

    fix: collapse gui controls by default

--- apps/web/src/components/hooks/useBaryonControls.js
+++ apps/web/src/components/hooks/useBaryonControls.js
@@ -38,7 +38,7 @@ export function useBaryonControls() {
         return;
       }
 
-      pane = /** @type {typeof pane} */ (new Pane({ title: "Baryon", expanded: true }));
+      pane = /** @type {typeof pane} */ (new Pane({ title: "Baryon", expanded: false }));
       pane.element.style.position = "fixed";
       pane.element.style.top = "1rem";
       pane.element.style.right = "1rem";
